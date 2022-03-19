72feba99549e2274049e6978104e743b0a56ad31
LuthyX <peteradegoke81@gmail.com>
2022-03-19 09:47:42 +0100

    reducing the size of an image  without losing the aspect ratio

--- images.py
+++ images.py
@@ -1,11 +1,16 @@
 from PIL import Image, ImageFilter
-img = Image.open('./pokedex/pikachu.jpg')
+# img = Image.open('./pokedex/pikachu.jpg')
 
 
-filtered_img= img.convert('L')
-crooked = filtered_img.rotate(180)
-box= (100, 100, 400, 400)
-region = filtered_img.crop(box)
-resize= filtered_img.resize((300,300))
-region.save("grey.png", 'png')
-region.show() 
+# filtered_img= img.convert('L')
+# crooked = filtered_img.rotate(180)
+# box= (100, 100, 400, 400)
+# region = filtered_img.crop(box)
+# resize= filtered_img.resize((300,300))
+# region.save("grey.png", 'png')
+# region.show() 
+
+img2=Image.open('./astro.jpg')
+img2.thumbnail((400,400))
+img2.save('thumbnail.jpg')
+img2.show()
